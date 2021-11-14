794832bbb50cd08b43750c98391a7d85f2a3cccb
Josh Crowther <josh.crowther@akunacapital.com>
2021-11-14 06:06:43 -0600

    WIP: Prettier

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export function sum(...numbers) {
   const parsed = numbers.map(num => parseInt(num, 10));
 
   if (parsed.some(num => isNaN(num))) {
-    throw new Error('Invalid numbers passed to `sum`');
+    throw new Error("Invalid numbers passed to `sum`");
   }
 
   return parsed.reduce((a, b) => a + b);
@@ -16,18 +16,23 @@ export function sum(...numbers) {
  */
 export function difference(...numbers) {
   if (numbers.length !== 2) {
-    throw new Error('Incorrect number of parameters passed to `difference`');
+    throw new Error("Incorrect number of parameters passed to `difference`");
   }
   let [start, end] = numbers;
-  if (start === undefined || start === null || end === undefined || end === null) {
-    throw new Error('`start` and `end` values not properly passed');
+  if (
+    start === undefined ||
+    start === null ||
+    end === undefined ||
+    end === null
+  ) {
+    throw new Error("`start` and `end` values not properly passed");
   }
 
   start = parseInt(start, 10);
   end = parseInt(end, 10);
 
-  if (isNaN(start)) throw new Error('`start` is not a valid integer');
-  if (isNaN(end)) throw new Error('`end` is not a valid integer');
+  if (isNaN(start)) throw new Error("`start` is not a valid integer");
+  if (isNaN(end)) throw new Error("`end` is not a valid integer");
 
   return Math.abs(end - start);
 }
